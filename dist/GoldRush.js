a2bc46da07538543da9f0ca6c114b61bdc4b36a9
AviadC31 <aviadcoh1@gmail.com>
2024-06-29 14:42:46 +0300

    maintenance

--- dist/GoldRush.js
+++ dist/GoldRush.js
@@ -1,54 +1,55 @@
 class GoldRush extends Matrix {
-    constructor() {
-        super()
-        this.score = { player1: 0, player2: 0 }
-        this.coin = `./gifs/4.gif`
-        this.point = 'https://i.stack.imgur.com/Vkq2a.png'
-        this.player1 = './gifs/pacmanPlayer.gif'
-        this.wall = './pics/9.jpg'
-    }
-    // ${Math.floor(10 * Math.random())}
-    // axios.get('https://localhost:3000/api/randomCoin')
-    generateMatrix = (rows, cols) => {
-        for (let r = 0; r < rows; r++) {
-            this.matrix.push([])
-            for (let c = 0; c < cols; c++) {
-                const random = Math.floor(100 * Math.random())
-                if (random < 60) this.matrix[r].push(this.point)
-                else
-                if (random < 90) this.matrix[r].push(this.coin)
-                else this.matrix[r].push(this.wall)
-            }
-        }
-        return this.matrix
-    }
-    
-    loadBoard = (x, y) => this.generateMatrix(x, y)
+   constructor() {
+      super()
+      this.score = { player1: 0, player2: 0 }
+      this.coin = `./gifs/${Math.floor(Math.random() * 8)}.gif`
+      this.point = '/pics/blank.png'
+      this.player1 = './gifs/pacmanPlayer.gif'
+      this.wall = './pics/9.jpg'
+   }
+   // ${Math.floor(10 * Math.random())}
+   // axios.get('https://localhost:3000/api/randomCoin')
+   generateMatrix = (rows, cols) => {
+      for (let r = 0; r < rows; r++) {
+         this.matrix.push([])
+         for (let c = 0; c < cols; c++) {
+            const random = Math.floor(100 * Math.random())
+            if (random < 60) this.matrix[r].push(this.point)
+            else if (random < 90) this.matrix[r].push(this.coin)
+            else this.matrix[r].push(this.wall)
+         }
+      }
+      return this.matrix
+   }
 
-    movePlayer = (player, direction) => {
-        if (!this.findCoordinate(player)) return
-        const { x, y } = this.findCoordinate(player)
-        switch (direction) {
-            case 'down': if (this.movingAndScoring(x + 1, y, player)) return
-                break
-            case 'up': if (this.movingAndScoring(x - 1, y, player)) return
-                break
-            case 'left': if (this.movingAndScoring(x, y - 1, player)) return
-                break
-            case 'right': if (this.movingAndScoring(x, y + 1, player)) return
-                break
-        }
-        this.alter(x, y, this.point)
-    }
+   loadBoard = (x, y) => this.generateMatrix(x, y)
 
-    movingAndScoring = (x, y, player) => {
-        if (!this.matrix[x]) return true
-        if (this.matrix[x][y] === this.coin) {
-            if (player === this.player1) this.score.player1 += 10
-            else this.score.player2 += 10
-        } else if (this.matrix[x][y] !== this.point) return true
-        this.alter(x, y, player)
-    }
+   movePlayer = (player, direction) => {
+      if (!this.findCoordinate(player)) return
+      const { x, y } = this.findCoordinate(player)
+      switch (direction) {
+         case 'down':
+            if (this.movingAndScoring(x + 1, y, player)) return
+            break
+         case 'up':
+            if (this.movingAndScoring(x - 1, y, player)) return
+            break
+         case 'left':
+            if (this.movingAndScoring(x, y - 1, player)) return
+            break
+         case 'right':
+            if (this.movingAndScoring(x, y + 1, player)) return
+            break
+      }
+      this.alter(x, y, this.point)
+   }
+
+   movingAndScoring = (x, y, player) => {
+      if (!this.matrix[x]) return true
+      if (this.matrix[x][y] === this.coin) {
+         if (player === this.player1) this.score.player1 += 10
+         else this.score.player2 += 10
+      } else if (this.matrix[x][y] !== this.point) return true
+      this.alter(x, y, player)
+   }
 }
-
-
--- dist/main.js
+++ dist/main.js
@@ -6,159 +6,159 @@ const socket = io()
 loadnewGame = () => location.reload()
 
 displayBoard = () => {
-
-    let dimension = $("#dimension").val()
-    board.loadBoard(Math.floor(dimension / 2), dimension)
-    board.alter(Math.floor(dimension / 2) - 1, dimension - 1, playerB)
-    board.alter(0, 0, playerA)
-    renderer.render(board.score, 'score')
-    renderer.render(board.matrix, 'board')
-    $("#playNow").empty()
-    $("body").css("background-image", "url(.)")
-
+   let dimension = $('#dimension').val()
+   board.loadBoard(Math.floor(dimension / 2), dimension)
+   board.alter(Math.floor(dimension / 2) - 1, dimension - 1, playerB)
+   board.alter(0, 0, playerA)
+   renderer.render(board.score, 'score')
+   renderer.render(board.matrix, 'board')
+   $('#playNow').empty()
+   $('body').css('background-image', 'url(.)')
 }
 
 displayMultiBoard = () => {
-
-    let dimension = $("#dimension").val()
-    board.loadBoard(Math.floor(dimension / 2), dimension)
-    board.alter(Math.floor(dimension / 2) - 1, dimension - 1, playerB)
-    board.alter(0, 0, playerA)
-    socket.emit('board', { matrix: board.matrix, score: board.score })
+   let dimension = $('#dimension').val()
+   board.loadBoard(Math.floor(dimension / 2), dimension)
+   board.alter(Math.floor(dimension / 2) - 1, dimension - 1, playerB)
+   board.alter(0, 0, playerA)
+   socket.emit('board', { matrix: board.matrix, score: board.score })
 }
 
 gameOver = () => {
-
-    if (board.score.player1 >= board.matrix.length * board.matrix[0].length * 1.5) return 'pacman'
-    if (board.score.player2 >= board.matrix.length * board.matrix[0].length * 1.5) return 'sonic'
-    if (!(JSON.stringify(board.findCoordinate(board.coin))))
-        return board.score.player1 >= board.score.player2 ? 'pacman' : 'sonic'
+   if (board.score.player1 >= board.matrix.length * board.matrix[0].length * 1.5) return 'pacman'
+   if (board.score.player2 >= board.matrix.length * board.matrix[0].length * 1.5) return 'sonic'
+   if (!JSON.stringify(board.findCoordinate(board.coin)))
+      return board.score.player1 >= board.score.player2 ? 'pacman' : 'sonic'
 }
 
 multiGameOver = (player) => {
-    let winneris = ''
-    socket.emit('gameOver', {
-        player: player,
-        outOfcoins: (!(JSON.stringify(board.findCoordinate(board.coin))))
-    })
+   let winneris = ''
+   socket.emit('gameOver', {
+      player: player,
+      outOfcoins: !JSON.stringify(board.findCoordinate(board.coin)),
+   })
 }
 
 loadPage = () => {
+   socket.emit('player', 'player')
+   socket.on('player', (player) => {
+      $('#multiplayer').on('click', () => {
+         $('span').empty()
 
-    socket.emit('player', 'player')
-    socket.on('player', player => {
+         renderer.waitingRoom()
 
-        $("#multiplayer").on('click', () => {
-            $("span").empty()
+         socket.emit('gameReady', 'gameReady')
+         socket.on('gameReady', (gameReady) => {
+            if (player === 'host') renderer.playNow()
 
-            renderer.waitingRoom()
-
-            socket.emit('gameReady', 'gameReady')
-            socket.on('gameReady', gameReady => {
-                if (player === 'host') renderer.playNow()
-
-                $(".playBt").on('click', () => {
-                    displayMultiBoard()
-                })
+            $('.playBt').on('click', () => {
+               displayMultiBoard()
             })
+         })
 
-            socket.on('board', multiBoard => {
-                if (player === 'guest') {
-                    board.matrix = [ ...multiBoard.matrix ]
-                    board.score = {...multiBoard.score}
-                }
-                $("#playNow").empty()
-                $("body").css("background-image", "url(.)")
-                renderer.render(multiBoard.score, 'score')
-                renderer.render(multiBoard.matrix, 'board')
+         socket.on('board', (multiBoard) => {
+            if (player === 'guest') {
+               board.matrix = [...multiBoard.matrix]
+               board.score = { ...multiBoard.score }
+            }
+            $('#playNow').empty()
+            $('body').css('background-image', 'url(.)')
+            renderer.render(multiBoard.score, 'score')
+            renderer.render(multiBoard.matrix, 'board')
+         })
+
+         $(document).on('keypress', function (e) {
+            multiGameOver(player)
+            socket.on('winnerIs', (winnerIs) => {
+               // console.log(winnerIs)
+               //     if (winnerIs === 'host') winneris = 'pacman'
+               //     if (winnerIs === 'guest') winneris = 'sonic'
+               // })
+               // if (winner === 'sonic' || winner === 'pacman') {
+               //     socket.emit('ceremony', winner)
+               //     socket.on('ceremony', ceremony => {
+               renderer.displayScreen('gifs/winner.gif')
+               renderer.appendBody(winnerIs)
+               return
             })
+            // }
+            console.log(e.key)
 
-            $(document).on("keypress",function (e) {
-               multiGameOver(player)
-                socket.on('winnerIs', winnerIs => {
-                    // console.log(winnerIs)
-                //     if (winnerIs === 'host') winneris = 'pacman'
-                //     if (winnerIs === 'guest') winneris = 'sonic'
-                // })
-                // if (winner === 'sonic' || winner === 'pacman') {
-                //     socket.emit('ceremony', winner)
-                //     socket.on('ceremony', ceremony => {
-                        renderer.displayScreen('gifs/winner.gif')
-                        renderer.appendBody(winnerIs)
-                        return
-                    })
-                // }
-                switch (e.key) {
-                    case 'w': board.movePlayer(player === 'host' ? playerA : playerB, "up")
-                        break
-                    case 's': board.movePlayer(player === 'host' ? playerA : playerB, "down")
-                        break
-                    case 'a': board.movePlayer(player === 'host' ? playerA : playerB, "left")
-                        break
-                    case 'd': board.movePlayer(player === 'host' ? playerA : playerB, "right")
-                        break
-                }
+            switch (e.key) {
+               case 'w':
+                  board.movePlayer(player === 'host' ? playerA : playerB, 'up')
+                  break
+               case 's':
+                  board.movePlayer(player === 'host' ? playerA : playerB, 'down')
+                  break
+               case 'a':
+                  board.movePlayer(player === 'host' ? playerA : playerB, 'left')
+                  break
+               case 'd':
+                  board.movePlayer(player === 'host' ? playerA : playerB, 'right')
+                  break
+            }
 
-                socket.emit('movement', { matrix: board.matrix, score: board.score })
-                socket.on('movement', movement => {
-                    board.matrix = [...movement.matrix]
-                    board.score.player1 = movement.score.player1
-                    board.score.player2 = movement.score.player2
-                    renderer.render(board.score, 'score')
-                    renderer.render(board.matrix, 'board')
-                })
+            socket.emit('movement', { matrix: board.matrix, score: board.score })
+            socket.on('movement', (movement) => {
+               board.matrix = [...movement.matrix]
+               board.score.player1 = movement.score.player1
+               board.score.player2 = movement.score.player2
+               renderer.render(board.score, 'score')
+               renderer.render(board.matrix, 'board')
             })
+         })
+      })
+   })
+   $('#singleplayer').on('click', async () => {
+      $('span').empty()
+      renderer.playNow()
 
-        })
-    })
-    $("#singleplayer").on('click', async () => {
-        $("span").empty()
-        renderer.playNow()
+      $('.playBt').on('click', async () => {
+         displayBoard()
 
-        $(".playBt").on('click', async () => {
+         $(document).on('keydown', function (e) {
+            const winner = gameOver()
 
-            displayBoard()
+            if (winner == 'sonic' || winner == 'pacman') {
+               renderer.displayScreen('gifs/winner.gif')
+               renderer.appendBody(winner)
+               return
+            }
+            switch (e.key) {
+               case 'w':
+                  board.movePlayer(playerA, 'up')
+                  break
+               case 's':
+                  board.movePlayer(playerA, 'down')
+                  break
+               case 'a':
+                  board.movePlayer(playerA, 'left')
+                  break
+               case 'd':
+                  board.movePlayer(playerA, 'right')
+                  break
+               case '8':
+               case 'ArrowUp':
+                  board.movePlayer(playerB, 'up')
+                  break
+               case '5':
+               case 'ArrowDown':
+                  board.movePlayer(playerB, 'down')
+                  break
+               case '4':
+               case 'ArrowLeft':
+                  board.movePlayer(playerB, 'left')
+                  break
+               case '6':
+               case 'ArrowRight':
+                  board.movePlayer(playerB, 'right')
+                  break
+            }
 
-            $(document).on("keypress",function (e) {
-
-                const winner = gameOver()
-
-                if (winner == 'sonic' || winner == 'pacman') {
-
-                    renderer.displayScreen('gifs/winner.gif')
-                    renderer.appendBody(winner)
-                    return
-                }
-                switch (e.key) {
-                    case 'w': board.movePlayer(playerA, "up")
-                        break
-                    case 's': board.movePlayer(playerA, "down")
-                        break
-                    case 'a': board.movePlayer(playerA, "left")
-                        break
-                    case 'd': board.movePlayer(playerA, "right")
-                        break
-                    case '8': board.movePlayer(playerB, "up")
-                        break
-                    case '5': board.movePlayer(playerB, "down")
-                        break
-                    case '4': board.movePlayer(playerB, "left")
-                        break
-                    case '6': board.movePlayer(playerB, "right")
-                        break
-                }
-
-                renderer.render(board.score, 'score')
-                renderer.render(board.matrix, 'board')
-            })
-        })
-
-    })
-
+            renderer.render(board.score, 'score')
+            renderer.render(board.matrix, 'board')
+         })
+      })
+   })
 }
-
-
-
-
-
-
